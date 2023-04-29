3f4952597f44cbdfb59f5fb9b6f7d56320c3f83d
GVxK <guilhermevxk@gmail.com>
2023-04-29 00:29:44 -0300

    a

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ let tailLength = 2
 // add fruit
 let appleX = 5
 let appleY = 5
-let hue = 0
+let hue = 1
+const particlesArray = []
 //score
 let score = 0
 
@@ -133,22 +134,80 @@ function drawApple(){
     ctx.fill()
     
 }
-function animateApple() {
-    
-    if (hue > 30) {
-        hue--
-    } else if (hue == 0) {
-        hue++
-    }
-    requestAnimationFrame(animateApple)
-}
-animateApple()
-
 function checkCollision(){
     if(appleX == headX && appleY == headY){
         appleX = Math.floor(Math.random()*(canvas.width/tileCount))
         appleY = Math.floor(Math.random()*(canvas.height/tileCount))
         tailLength++
-   
+        for (i=0;i<tailLength*2;i++){
+            particlesArray.push(new Particle())
+        }
     }
 }
+
+// const mouse = {
+//     x:undefined,
+//     y:undefined
+// }
+
+
+// canvas.addEventListener("mousemove", function(event){
+//     mouse.x = event.x
+//     mouse.y = event.y
+//     for (i=0;i<3;i++){
+//         particlesArray.push(new Particle())
+//     }
+// })
+
+class Particle {
+    constructor(){
+        this.x = headX * tileCount
+        this.y = headY * tileCount
+        this.size = Math.random() *tailLength/10 + 1
+        this.speedX = Math.random() *3 - 1.5
+        this.speedY = Math.random() *3 - 1.5
+        this.color = `hsl(${hue}, 100%, 50%)`
+    }
+    update(){
+        this.x += this.speedX
+        this.y += this.speedY
+        if (this.size >0.2) this.size -= 0.05
+    }
+    draw() {
+        ctx.fillStyle = this.color
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI *2)
+        ctx.fill()
+    }
+    
+}
+
+
+
+function handleParticles(){
+    for(let i = 0; i<particlesArray.length; i++){
+        particlesArray[i].update()
+        particlesArray[i].draw()
+        if (particlesArray[i].size <= 0.3) {
+            particlesArray.splice(i, 1)
+            i--
+        }
+    }
+}
+
+// function drawCircle(){
+//     ctx.fillStyle = "blue"
+//     ctx.beginPath()
+//     ctx.arc(mouse.x, mouse.y, 20, 0, 25)
+//     ctx.fill()
+// }
+
+
+function animate(){
+    hue++
+    if (hue>30) {hue = 0}
+    handleParticles()
+    requestAnimationFrame(animate)
+
+}
+animate()
